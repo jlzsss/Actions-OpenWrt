766987945d5a8522d89f9c3be19a5150b463422e
jlzsss <jlz19821031@gmail.com>
2023-05-03 11:08:35 +0800

    Update diy-part1-openwrt.sh

--- diy-part1-openwrt.sh
+++ diy-part1-openwrt.sh
@@ -18,8 +18,8 @@ git clone https://github.com/kuoruan/luci-app-kcptun.git package/luci-app-kcptun
 git clone https://github.com/jlzsss/openwrt-kcptun.git package/kcptun
 git clone https://github.com/jlzsss/openwrt-miredo.git package/miredo
 git clone https://github.com/Mleaf/openwrt-mwol.git package/mwol
-git clone https://github.com/yichya/luci-app-xray.git package/luci-app-xray
-git clone https://github.com/yichya/openwrt-xray.git package/openwrt-xray
+git clone https://github.com/xiechangan123/luci-app-xray.git package/luci-app-xray-fw4
+git clone https://github.com/xiechangan123/luci-i18n-xray-zh-cn.git package/luci-i18n-xray-zh-cn.git
 git clone -b luci2 https://github.com/jlzsss/luci-app-v2ray.git package/luci-app-v2ray
 git clone https://github.com/frainzy1477/luci-app-trojan.git package/luci-app-trojan
 git clone -b test https://github.com/frainzy1477/luci-app-clash.git package/luci-app-clash
@@ -41,8 +41,8 @@ git clone https://github.com/jlzsss/luci-app-cloudflarespeedtest.git package/luc
 # git clone https://github.com/jlzsss/smartdns.git package/feeds/packages/smartdns
 # git clone https://github.com/jlzsss/luci-app-smartdns.git package/feeds/luci/luci-app-smartdns
 git clone https://github.com/jlzsss/libgd.git package/feeds/packages/libgd
-# git clone https://github.com/v2rayA/v2raya-openwrt.git package/v2raya-openwrt
-git clone https://github.com/jlzsss/v2raya-openwrt.git package/v2raya-openwrt
+git clone https://github.com/v2rayA/v2raya-openwrt.git package/v2raya-openwrt
+# git clone https://github.com/jlzsss/v2raya-openwrt.git package/v2raya-openwrt
 git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/sbwml/luci-app-mosdns package/mosdns
